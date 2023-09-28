ed43c22868491d32135b201e38728a6f9e866962
Zuza <144389463+T-zuza@users.noreply.github.com>
2023-09-28 21:40:13 +0400

    Add files via upload

---
+++ DAY1/homeworks/homeworks1.py
@@ -0,0 +1,97 @@
+time=(24*60)*60
+print(time)
+
+print(2.71)
+print(float)
+
+print("I am a string")
+
+print("Theme " + "Park")
+
+print("28"+"6")
+print(28+6)
+
+print(2 * 3)
+
+#Drag and drop to work out the number of hours in 330 minutes.
+print(330/60)
+
+print("cup" +"cake")
+
+#Fill in the blanks to output the result of multiplying 5 by 3.
+print(5*3)
+
+print(2*2)
+
+print(10-3)
+
+print("Name:")
+print("Mary")
+print("Balance:")
+print(500)
+
+print("Balance:", 5000)
+
+print("Score:",95)
+
+print("Iron", "Man", sep ="-")  
+
+print("Spain", "Madrid",sep="-")
+
+#The line below adds two numbers
+print(47+14)
+
+
+#Outputs a message
+print("Have a good day!")
+
+#Calculate the number of hours in a week
+print(24*7)
+
+points=15
+print(points)
+
+price= 15
+print(price*3)
+
+year = 2023
+print(year + 5)
+
+x = 8
+print(x)
+x = 41
+print(x)
+
+age = 42
+age = 24
+print(age)
+
+x = 9
+y = 5
+
+print(x*y)
+print(x/y)
+
+x = 10
+y = 2
+print(x+y)
+
+result = 798
+currency = "$"
+print("Total:", currency, result)
+
+years =15
+print(years*12)
+ 
+a = 2
+b = 4
+print(a+b)
+
+#I don't undarstend how 
+print("42" +"5")
+
+#
+miles=(7425)
+speed=(550)
+print(miles/speed)
+
