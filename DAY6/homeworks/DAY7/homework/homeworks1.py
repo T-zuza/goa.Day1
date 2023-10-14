0f7cc498261df568c3447ebef50764c98ed91525
Zuza <144389463+T-zuza@users.noreply.github.com>
2023-10-14 23:20:36 +0400

    Add files via upload

---
+++ DAY6/homeworks/DAY7/homework/homeworks1.py
@@ -0,0 +1,27 @@
+shopping_cart = [
+  "laptop", 
+  "smartphone", 
+  "headphones", 
+  "backpack"
+]
+print(shopping_cart)
+
+ages = [18,31,42]
+print(ages)
+
+last_calls = ["Mom", "Dave", "Lawyer"]
+print(last_calls[0])
+print(last_calls[1])
+
+animals = ["cat" , "dog" , "bird"]
+print (animals [2])
+
+animals = ["cat" , "dog" , "bird"]
+my_pet=animals[1]
+print(my_pet)
+
+nums = [8, 6, 19]
+nums[0] = 1
+nums[1] = 2
+nums[2] = 3
+print(nums)
---
+++ DAY6/homeworks/DAY7/homworks2.py
@@ -0,0 +1,24 @@
+# დავალება 1:
+# შექმენით სია სადაც გადასცემთ რაზმის ლიდერების სახელს და გვარს     ელემენტებად და გამოიტანთ თქვენი რაზმის ლიდერის სახელს
+
+squad_lead = ["molodini" , "janezashvili" , "tkeshelashvili" , "philishvili"]
+print(squad_lead[0])
+
+# დავალება 2:
+# შექმენით თქვენი რაზმის წევრებისგან სია და დაპრინტეთ შუა 5 რაზმის წევრი(slicing)
+
+squad_memb = ["guriko","tini","luka","giorgi","tekla","rezi","davit","mirian","rati"]
+print(squad_memb[2])
+print(squad_memb[3])
+print(squad_memb[4])
+print(squad_memb[5])
+print(squad_memb[6])
+
+# დავალება3:
+# შექმენით სია სადაც იქნება რამდენიმე ხილის სახელი და 1 ბოსტნეული, თქვენ უნდა შეცვალოთ ეს 1 ბოსტნეული სხვა რაიმე ხილით და დაპრინტოთ სია
+
+fruit_shelf = ["apple","banana","grapes","pineapple","orange","potato","peach","mango"]
+plum = fruit_shelf[5]
+print(plum)
+
+
